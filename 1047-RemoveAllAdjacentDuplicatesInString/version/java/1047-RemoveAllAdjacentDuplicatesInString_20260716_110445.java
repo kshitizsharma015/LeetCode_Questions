767b59182b691e5f803ef3db0c691dc0ff8009d0
kshitizsharma015 <142567075+kshitizsharma015@users.noreply.github.com>
2026-07-16 11:04:47 +0530

    Create 1047-RemoveAllAdjacentDuplicatesInString

---
+++ 1047-RemoveAllAdjacentDuplicatesInString/version/java/1047-RemoveAllAdjacentDuplicatesInString_20260716_110445.java
@@ -0,0 +1,20 @@
+// Last updated: 7/16/2026, 11:04:45 AM
+// Use stack for this because stack can se adjecent equal value
+1class Solution {
+2    public String removeDuplicates(String s) {
+3        String x = "";
+4        Stack<Character> stack = new Stack<>();
+5        for(char ch : s.toCharArray()){
+6            if(!stack.isEmpty() && stack.peek()==ch){
+7                stack.pop();
+8            }
+9            else{
+10                stack.push(ch);
+11            }
+12        }
+13        while(!stack.isEmpty()){
+14            x=stack.pop() + x;
+15        }
+16        return x;
+17    }
+18}
